cd1d5120392fee32d2236d3a4cd4407618faed51
todo <sl3003505@gmail.com>
2023-04-24 21:44:53 +0300

    уйцу

--- src/js/EditCards.js
+++ src/js/EditCards.js
@@ -7,21 +7,3 @@ export function editCards(){
 	});
 });
 }
-
-
-// const result=document.querySelector(".cards-box")
-// 	for(let key in content){
-// 		let category = content[key].category;
-// 		category = category.replace(/[' ]/g, '-');
-// 		const itemId = `${category}-${key}`;
-// 		result.innerHTML+=`<div id="${itemId}" class="cards-box__item">
-// 		<i class="fa-solid fa-pen-to-square cards-box__icons"></i>
-// 		<img class="cards-box__img"  src="${content[key].image}" alt="">
-// 		<hr class="cards-box__hr">
-// 		<div class="cards-box__about">
-// 			<p class="cards-box__name">${content[key].title}</p>
-// 			<p class="cards-box__price"><span>${content[key].price}</span>&#36</p>
-// 		</div>
-// 	</div>`
-// 	}
-// 	editCards();
