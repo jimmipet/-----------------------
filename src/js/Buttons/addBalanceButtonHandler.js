924581d6a2a0d44217e6e6dabe53aab5dad30dfe
todo <sl3003505@gmail.com>
2023-04-26 22:29:50 +0300

    42134124

---
+++ src/js/Buttons/addBalanceButtonHandler.js
@@ -0,0 +1,28 @@
+
+export function addBalanceButtonHandler() {
+	const format = document.querySelector('.format');
+	const close = document.querySelector('.format-btn__back');
+	const add = document.querySelector('.format-btn__add');
+	const balance = document.querySelector('.balance-value');
+  
+	// Открыть форму начисления баланса
+	const credit = document.querySelector('.btn-credit');
+	credit.addEventListener('click', () => {
+	  format.style.display = 'block';
+	});
+  
+	// Закрыть форму начисления баланса
+	close.addEventListener('click', () => {
+	  format.style.display = 'none';
+	});
+  
+	// Начислить баланс
+	add.addEventListener('click', () => {
+	  const valueBalance = Number(balance.textContent);
+	  const valueInput = Number(document.querySelector('.format-input').value);
+	  const sum = valueBalance + valueInput;
+	  balance.textContent = sum;
+  
+	  format.style.display = 'none';
+	});
+  }
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import { openCredit, closeCredit, add } from './buttons/AddBalanceButton.js';
+import { addBalanceButtonHandler } from './buttons/addBalanceButtonHandler.js';
 import { goods, styleDropdown } from './buttons/buttonALLgoods.js';
 import { editCards } from './buttons/editCards.js'
 import { createCardsItem } from './markupHelpers/createCardsItem.js'
