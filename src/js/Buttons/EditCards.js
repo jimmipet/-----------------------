f0e879de69a87ccc0e747adc203ebf9cd343d814
todo <sl3003505@gmail.com>
2023-04-28 17:44:12 +0300

    1231123

--- src/js/Buttons/EditCards.js
+++ src/js/Buttons/EditCards.js
@@ -1,14 +1,9 @@
 //кнопка редактирования карточки 
 export function editCards(){
 	const elements = document.querySelectorAll(".cards-box__item i");
-	const edit = document.querySelector('.edit');
-	const back = document.querySelector('.edit-btn__back');
 	elements.forEach(function(element) {
 	element.addEventListener("click", function() {
-		edit.style.display = 'block';
-	});
-	back.addEventListener('click', ()=>{
-		edit.style.display = 'none';
+		console.log("click")
 	});
 });
 }
